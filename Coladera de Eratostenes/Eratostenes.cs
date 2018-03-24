aaf0bd63e7df1e8c7ae9ac792f28e90a40ab996e
Hectorolon <hrolon@ucol.mx>
2018-03-24 00:15:13 -0600

    first commit

---
+++ Coladera de Eratostenes/Eratostenes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coladera_de_Eratostenes
+{
+    class Eratostenes
+    {
+        public static string obtenerPrimo(int tamaño)
+        {
+            bool [] _v = new bool [tamaño];
+            string primo = "";
+            //Se llena el vector con verdadero
+            for (int i = 0; i < tamaño; i++)
+                _v[i] = true;
+            //Tachar los multiplos
+            for (int i = 2; i < tamaño; i++)
+            {
+                //Solo si esta verdadero 
+                if (_v[i])
+                {
+                    for (int j = 2; i * j < tamaño; j++)
+                        _v[i * j] = false;
+                }
+            }
+            for(int i=2; i<tamaño; i++)
+            {
+                if (_v[i])
+                    primo += i.ToString() + Environment.NewLine;
+            }
+            return primo;
+        }
+    }
+}
---
+++ Coladera de Eratostenes/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Coladera_de_Eratostenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrimos = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrimos
+            // 
+            this.btnPrimos.Location = new System.Drawing.Point(78, 12);
+            this.btnPrimos.Name = "btnPrimos";
+            this.btnPrimos.Size = new System.Drawing.Size(108, 23);
+            this.btnPrimos.TabIndex = 0;
+            this.btnPrimos.Text = "Numeros Primos";
+            this.btnPrimos.UseVisualStyleBackColor = true;
+            this.btnPrimos.Click += new System.EventHandler(this.btnPrimos_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(86, 55);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(100, 194);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnPrimos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrimos;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
